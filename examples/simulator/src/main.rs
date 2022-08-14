4085d5634f15335ce76e24c5c27a3839b666525b
Finomnis <finomnis@gmail.com>
2022-08-15 01:37:52 +0200

    Add graphics simulator to example

--- examples/simulator/src/main.rs
+++ examples/simulator/src/main.rs
@@ -1,5 +1,29 @@
-use u8g2_fonts::hello_world;
+use embedded_graphics::{
+    pixelcolor::Rgb888,
+    prelude::*,
+    primitives::{Circle, PrimitiveStyle},
+};
+use embedded_graphics_simulator::{OutputSettings, SimulatorDisplay, SimulatorEvent, Window};
 
-fn main() {
-    hello_world();
+fn main() -> Result<(), core::convert::Infallible> {
+    let mut display: SimulatorDisplay<Rgb888> = SimulatorDisplay::new(Size::new(800, 480));
+    let mut window = Window::new("Click to move circle", &OutputSettings::default());
+
+    let position = Point::new(200, 200);
+    Circle::with_center(position, 200)
+        .into_styled(PrimitiveStyle::with_fill(Rgb888::RED))
+        .draw(&mut display)?;
+
+    'running: loop {
+        window.update(&display);
+
+        for event in window.events() {
+            match event {
+                SimulatorEvent::Quit => break 'running,
+                _ => {}
+            }
+        }
+    }
+
+    Ok(())
 }
