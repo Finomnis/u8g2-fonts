00742e053089f2d5a6a11960e355a11d5f3ecfdd
Finomnis <finomnis@gmail.com>
2022-08-16 00:00:06 +0200

    Fix minor bug in char range

--- tools/generate_fonts_file/src/u8_compression.rs
+++ tools/generate_fonts_file/src/u8_compression.rs
@@ -6,7 +6,7 @@ pub fn write_byte(out: &mut Vec<u8>, c: u8) {
         b'\t' => out.extend_from_slice(br"\t"),
         b'"' => out.extend_from_slice(br#"\""#),
         b'\\' => out.extend_from_slice(br"\\"),
-        20..=126 => out.push(c),
+        32..=126 => out.push(c),
         _ => out.extend_from_slice(format!("\\x{:02x}", c).as_bytes()),
     }
 }
