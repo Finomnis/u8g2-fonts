6b035edc16c625075e496ec54825520546952cda
Finomnis <finomnis@gmail.com>
2022-08-18 00:58:25 +0200

    Enforce docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,13 @@
+//!
+//!
+
 #![no_std]
+#![deny(missing_docs)]
+#![doc(
+    issue_tracker_base_url = "https://github.com/Finomnis/tokio-graceful-shutdown/issues",
+    test(no_crate_inject, attr(deny(warnings))),
+    test(attr(allow(dead_code)))
+)]
 
 mod error;
 mod font;
@@ -11,6 +20,8 @@ mod renderer;
 /// A collection of [U8g2 fonts](https://github.com/olikraus/u8g2/wiki/fntlistall).
 ///
 /// Note that every font has a different license. For more information, read the [U8g2 License Agreement](https://github.com/olikraus/u8g2/blob/master/LICENSE).
+#[allow(non_camel_case_types)]
+#[allow(missing_docs)]
 pub mod fonts;
 
 pub use error::Error;
