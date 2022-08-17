cc03280a815d1f2ec264aa4c6e3b1c815a64dffc
Finomnis <finomnis@gmail.com>
2022-08-18 01:46:09 +0200

    Add text to example

--- examples/simulator/src/main.rs
+++ examples/simulator/src/main.rs
@@ -7,23 +7,23 @@ use embedded_graphics_simulator::{OutputSettings, SimulatorDisplay, SimulatorEve
 
 use u8g2_fonts::{fonts, FontRenderer};
 
-const FONT: FontRenderer = FontRenderer::new::<fonts::u8g2_font_lubBI14_tf>();
+const FONT: FontRenderer = FontRenderer::new::<fonts::u8g2_font_luRS24_tf>();
 
 fn main() -> Result<(), core::convert::Infallible> {
     let mut display: SimulatorDisplay<Rgb888> = SimulatorDisplay::new(Size::new(800, 480));
-    let mut window = Window::new("Text rendering demo", &OutputSettings::default());
+    let mut window = Window::new("Text Rendering Demo", &OutputSettings::default());
 
     let position = Point::new(200, 200);
     Circle::with_center(position, 200)
         .into_styled(PrimitiveStyle::with_fill(Rgb888::RED))
         .draw(&mut display)?;
 
-    Line::new(Point::new(0, 20), Point::new(40, 20))
-        .into_styled(PrimitiveStyle::with_stroke(Rgb888::GREEN, 1))
+    Line::new(Point::new(0, 50), Point::new(40, 50))
+        .into_styled(PrimitiveStyle::with_stroke(Rgb888::CSS_DARK_GREEN, 1))
         .draw(&mut display)?;
 
-    Line::new(Point::new(20, 0), Point::new(20, 40))
-        .into_styled(PrimitiveStyle::with_stroke(Rgb888::GREEN, 1))
+    Line::new(Point::new(20, 0), Point::new(20, 100))
+        .into_styled(PrimitiveStyle::with_stroke(Rgb888::CSS_DARK_GREEN, 1))
         .draw(&mut display)?;
 
     let advance = FONT
@@ -35,12 +35,21 @@ fn main() -> Result<(), core::convert::Infallible> {
             &mut display,
         )
         .unwrap();
+    FONT.render_glyph(
+        'ß',
+        Point::new(20 + advance as i32, 20),
+        Rgb888::CSS_YELLOW,
+        None, //Some(Rgb888::CSS_DARK_GRAY),
+        &mut display,
+    )
+    .unwrap();
+
     let advance = FONT
-        .render_glyph(
-            'ß',
-            Point::new(20 + advance as i32, 20),
-            Rgb888::CSS_YELLOW,
-            None, //Some(Rgb888::CSS_DARK_GRAY),
+        .render_text(
+            "Angh Lorem ipsum dolor sit amet. Amit consectetur adipiscing elit. 20%! a",
+            Point::new(20, 50),
+            Rgb888::CSS_ORANGE,
+            None,
             &mut display,
         )
         .unwrap();
