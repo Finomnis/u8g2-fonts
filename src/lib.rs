f95b8cfddf8c33af4be7a4e3da92aebc82df624e
Finomnis <finomnis@gmail.com>
2022-08-26 16:59:56 +0200

    More work on docs

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,43 @@
 //! For more information about the font licenses, read the [license agreement](https://github.com/olikraus/u8g2/blob/master/LICENSE) of U8g2.
 //!
 //!
-//! # Rendering
+//! # Usage
 //!
+//! The central struct in this crate is the [`FontRenderer`]. It can render one specific font.
+//!
+//! A [`FontRenderer`] can be constructed through [`FontRenderer::new()`](FontRenderer::new). Its generic argument [`Font`] specifies which font it will render.
+//!
+//! Note that [`FontRenderer::new()`] is `const`, so it can be crated as a global variable at compile time for optimal performance.
+//!
+//! ## Fonts
+//!
+//! The fonts are directly imported from [U8g2](https://github.com/olikraus/u8g2/wiki).
+//!
+//! More information about all the available fonts can be found in the [U8g2 wiki](https://github.com/olikraus/u8g2/wiki/fntlistall).
+//!
+//!
+//! ## Content Types
+//!
+//! Once constructed, the [`FontRenderer`] can render [the following objects](Content):
+//!
+//! - Characters: `'a'`
+//! - Strings: `"Hello world!"`
+//! - Format Strings: `format_args!("Nice: {}", 69)`
+//!
+//! ## Vertical Positioning
+//!
+//!
+//!
+//!
+//!
+//! ## Bounding box calculation
+//!
+//! Additional to the [`render()`](FontRenderer::render) and [`render_aligned()`](FontRenderer::render_aligned) methods,
+//! there is also [`get_rendered_dimensions()`](FontRenderer::get_rendered_dimensions) and
+//! [`get_rendered_dimensions_aligned()`](FontRenderer::get_rendered_dimensions_aligned).
+//!
+//! Those functions behave almost identical to their `render` counterparts, but don't actually perform any rendering. This
+//! is very useful if the dimensions of the rendered text
 //!
 //!
 
