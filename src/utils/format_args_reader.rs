71682bf68e4f4474f3abead7f7e6057b08b6ce01
Finomnis <finomnis@gmail.com>
2022-08-27 13:09:30 +0200

    Add tests to format_args_reader and fix error

--- src/utils/format_args_reader.rs
+++ src/utils/format_args_reader.rs
@@ -3,6 +3,7 @@ use core::fmt::{Arguments, Write};
 pub struct FormatArgsReader<F, E> {
     action: F,
     error: Option<E>,
+    stopped: bool,
 }
 
 impl<F, E> FormatArgsReader<F, E>
@@ -13,6 +14,7 @@ where
         Self {
             action,
             error: None,
+            stopped: false,
         }
     }
 
@@ -31,13 +33,19 @@ where
     F: FnMut(char) -> Result<bool, E>,
 {
     fn write_str(&mut self, s: &str) -> core::fmt::Result {
-        if self.error.is_some() {
+        if self.error.is_some() || self.stopped {
             return Err(core::fmt::Error);
         }
         for char in s.chars() {
             match (self.action)(char) {
                 Ok(true) => (),
-                Ok(false) => break,
+                Ok(false) => {
+                    self.stopped = true;
+                    // Returning an error here doesn't
+                    // actually cause `process_args` to error,
+                    // it just stops the `write` call.
+                    return Err(core::fmt::Error);
+                }
                 Err(e) => {
                     self.error = Some(e);
                     return Err(core::fmt::Error);
@@ -74,5 +82,61 @@ where
             (self.action)(char);
         }
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate std;
+    use std::string::String;
+
+    use super::*;
+
+    #[test]
+    fn format_args_reader_provides_values() {
+        let mut output = String::new();
+
+        FormatArgsReader::new(|ch| -> Result<bool, &'static str> {
+            output.push(ch);
+            Ok(true)
+        })
+        .process_args(format_args!("Hello, w{}rld!", 0))
+        .unwrap();
+
+        assert_eq!(output, "Hello, w0rld!");
+    }
+
+    #[test]
+    fn format_args_reader_forwards_errors() {
+        let result = FormatArgsReader::new(|_| -> Result<bool, &'static str> { Err("Error!") })
+            .process_args(format_args!("Hello, w{}rld!", 0));
+
+        assert!(matches!(result, Err("Error!")));
+    }
+
+    #[test]
+    fn format_args_reader_stops_when_requested() {
+        let mut output = String::new();
+
+        FormatArgsReader::new(|ch| -> Result<bool, &'static str> {
+            output.push(ch);
+            Ok(output.len() < 5)
+        })
+        .process_args(format_args!("Hello, w{}rld!", 0))
+        .unwrap();
+
+        assert_eq!(output, "Hello");
+    }
+
+    #[test]
+    fn infallible_format_args_reader_provides_values() {
+        let mut output = String::new();
+
+        FormatArgsReaderInfallible::new(|ch| {
+            output.push(ch);
+        })
+        .process_args(format_args!("Hello, w{}rld!", 0));
+
+        assert_eq!(output, "Hello, w0rld!");
     }
 }
