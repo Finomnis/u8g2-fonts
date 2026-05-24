0ab1e560b69098f091a32fe1028f0febf303a3f1
Finomnis <finomnis@gmail.com>
2026-05-24 21:32:46 +0200

    Fix typo

--- src/renderer.rs
+++ src/renderer.rs
@@ -406,7 +406,7 @@ mod tests {
     #[test]
     fn new_from_raw_data() {
         let renderer_from_font = FontRenderer::new::<crate::fonts::u8g2_font_u8glib_4_tf>();
-        let renderer_from_slice =
+        let renderer_from_raw_data =
             FontRenderer::new_from_raw_data(crate::fonts::u8g2_font_u8glib_4_tf::DATA);
         assert_eq!(renderer_from_font.font, renderer_from_slice.font);
     }
