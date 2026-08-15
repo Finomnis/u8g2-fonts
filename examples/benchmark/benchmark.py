900930597555e86642b8702a1eb41567e3c01867
Finomnis <finomnis@gmail.com>
2026-08-15 23:31:09 +0200

    Attempt to fix CI

--- examples/benchmark/benchmark.py
+++ examples/benchmark/benchmark.py
@@ -3,14 +3,16 @@
 import subprocess
 from pathlib import Path
 import json
+from argparse import ArgumentParser
 
 SCRIPT_PATH = Path(__file__).resolve().parent
 
-MACHINES = [("thumbv6m-none-eabi", "microbit"),
-            ("thumbv7em-none-eabi", "mps2-an386")]
-
 
 def main():
+    parser = ArgumentParser()
+    parser.add_argument('target')
+    parser.add_argument('machine')
+    args = parser.parse_args()
 
     binary_names = []
     for p in (SCRIPT_PATH / 'src' / 'bin').iterdir():
@@ -18,26 +20,25 @@ def main():
             print(p.stem)
             binary_names.append(p.stem)
 
-    for (build_target, qemu_machine) in MACHINES:
-        subprocess.run(['cargo', 'build', '--release', '--target', build_target],
-                       cwd=SCRIPT_PATH, check=True)
+    subprocess.run(['cargo', 'build', '--release', '--target', args.target],
+                   cwd=SCRIPT_PATH, check=True)
 
-        for binary_name in binary_names:
+    for binary_name in binary_names:
 
-            print(f"Running benchmark '{binary_name}' on '{build_target}'...")
-            binary_path = SCRIPT_PATH / 'target' / build_target / 'release' / binary_name
+        print(f"Running benchmark '{binary_name}' ...")
+        binary_path = SCRIPT_PATH / 'target' / args.target / 'release' / binary_name
 
-            output = subprocess.run(['docker', 'run', '--rm',
-                                     '--mount', f'type=bind,src={binary_path},dst=/algo.firmware',
-                                     'ghcr.io/finomnis/qemu-embedded-bench:v0.3.0',
-                                     qemu_machine],
-                                    check=True,
-                                    stdout=subprocess.PIPE)
+        output = subprocess.run(['docker', 'run', '--rm',
+                                 '--mount', f'type=bind,src={binary_path},dst=/algo.firmware',
+                                 'ghcr.io/finomnis/qemu-embedded-bench:v0.3.0',
+                                 args.machine],
+                                check=True,
+                                stdout=subprocess.PIPE)
 
-            output = json.loads(output.stdout)
-            print(output)
-            assert (output['regions_started'] == 1)
-            assert (output['regions_completed'] == 1)
+        output = json.loads(output.stdout)
+        print(output)
+        assert (output['regions_started'] == 1)
+        assert (output['regions_completed'] == 1)
 
 
 if __name__ == "__main__":
